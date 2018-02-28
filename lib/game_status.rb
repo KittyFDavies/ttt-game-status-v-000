9431ce8aa6ed695df1cf450b2881eda5851460e1
Kitty Davies <kittyfdavies@gmail.com>
2018-02-28 23:17:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,9 +28,11 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]]
-      if win_combination.each do |position|
-        position == position_taken
-          return win_combination
+      if position_taken(board, win_combination[0]) == true
+        if position_taken(board, win_combination[1]) == true
+          if position_taken(board, win_combination[2]) == true
+            return win_combination
+          end
         end
       end
     end
