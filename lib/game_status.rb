40f52679630b68d78835ad80fbf3bfb9da4a8224
Kitty Davies <kittyfdavies@gmail.com>
2018-02-28 22:26:22 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,10 +18,8 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each { |win_combination|
     win_combination.each { |win_index|
-      if board[win_index[0]] == board[win_index[1]]
-          if board[win_index[1]] == board[win_index[2]]
-            return win_combination
-          end
+      if board[win_index[0]] == board[win_index[1]] &&  board[win_index[2]]
+        return win_combination
       end
     }
   }
@@ -43,6 +41,6 @@ def over?(board)
 
 end
 
-def winner
+def winner(board)
 
 end
