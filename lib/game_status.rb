d4e26d939d4d7b1ba1d4082c8d7d80fba1daebbf
Kitty Davies <kittyfdavies@gmail.com>
2018-02-28 20:59:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,6 +15,27 @@ WIN_COMBINATIONS = [
   [2, 4, 6] # r-to-l diagonal
 ]
 
-def won?
-
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    win_index_4 = win_combination[3]
+    win_index_5 = win_combination[4]
+    win_index_6 = win_combination[5]
+    win_index_7 = win_combination[6]
+    win_index_8 = win_combination[7]
+    win_index_9 = win_combination[8]
+  end
+  win_combination.each do |position|
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+  end
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination # return the win_combination indexes that won.
+  else
+    false
+  end
 end
+ 
