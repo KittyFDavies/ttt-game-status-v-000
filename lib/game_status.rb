a3621e8e441546b30e64abb182509c6ecfb0cac7
Kitty Davies <kittyfdavies@gmail.com>
2018-02-28 21:51:15 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,21 +16,12 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    win_index_4 = win_combination[3]
-    win_index_5 = win_combination[4]
-    win_index_6 = win_combination[5]
-    win_index_7 = win_combination[6]
-    win_index_8 = win_combination[7]
-  end
-  if (win_index_1[0] == "X" && win_index_1[1] == "X" && win_index_1[2] == "X") || (win_index_1[0] == "O" && win_index_1[1] == "O" && win_index_1[2] == "O")
-    return win_index_1
-  elsif win_index_2[0] == "X" && win_index_2[1] == "X" && win_index_2[2] == "X"
-    return win_index_2
-  end
+  WIN_COMBINATIONS.each { |win_combination|
+    win_combination.select { |win_index|
+      win_index.all? { ||  }
+    }
+  }
+  
 end
 
 def full?(board)
@@ -41,11 +32,11 @@ def full?(board)
   end
 end
 
-def draw?
+def draw?(board)
 
 end
 
-def over?
+def over?(board)
 
 end
 
