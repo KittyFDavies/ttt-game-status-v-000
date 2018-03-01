c8fdc5e297508600d4cafc14f47a2cbd66f6d1a6
Kitty Davies <kittyfdavies@gmail.com>
2018-03-01 00:56:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,7 +42,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || draw?(board) == true || full?(board) == true
+  if won?(board) || draw?(board) == true || full?(board) == true
     return true
   end
 end
