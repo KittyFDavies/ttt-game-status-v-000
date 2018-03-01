233d294b079c9d32d4d7de5592ee2c1a38b43970
Kitty Davies <kittyfdavies@gmail.com>
2018-03-01 00:27:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,8 +18,8 @@ WIN_COMBINATIONS = [
 def won?(board)
   if board.all do |space|
     space == " " || space == ""
-    return false
     end
+    return false
   elsif  WIN_COMBINATIONS.each do |win_combination|
       if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]]
         if position_taken?(board, win_combination[0]) == true
